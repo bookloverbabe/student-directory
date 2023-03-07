c081f9c84e4911ea9b88652db9e80d6a12cd3f17
bookloverbabe <solmaz@hotmail.co.uk>
2023-03-07 16:54:26 +0000

    First commit

---
+++ directory_exercises.rb
@@ -0,0 +1,46 @@
+# Create method which asks user to input student names
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  # Create an empty array to store our future students
+  students = []
+
+  # Get the first name
+  name = gets.chomp
+
+  # While the name is not empty, repeat this code
+  while !name.empty? do
+    # Add the student hash to the array. << is the shovel operator, which puts things into an array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # Get another name from the user
+    name = gets.chomp
+  end
+  # Return the array of students
+  students
+end
+
+# Create a method for the header
+def print_header
+  puts "The students of Villain Academy"
+  puts "-----------------"
+end
+
+# Create method to iterate over array of students. Student info now in a hash, so use key symbols within array [].
+def print(students)
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+# Create method to print footer
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# Run methods, its print(students) as it will be printing the students array
+students = input_students
+print_header
+print(students)
+print_footer(students)
