311d8d3112f7a0e3eae7738c2f836e78fb05d7b4
bookloverbabe <solmaz@hotmail.co.uk>
2023-03-08 20:17:26 +0000

    Step 8 question 6

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,21 +1,25 @@
 # Create method which asks user to input student names
 def input_students
   puts "Please enter the names of the students"
+  puts "Please enter your cohort"
   puts "To finish, just hit return twice"
 
   # Create an empty array to store our future students
   students = []
+  cohort = []
 
   # Get the first name
   name = gets.chomp
+  cohort = gets.chomp
 
   # While the name is not empty, repeat this code
   while !name.empty? do
     # Add the student hash to the array. << is the shovel operator, which puts things into an array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     # Get another name from the user
     name = gets.chomp
+    cohort = gets.chomp
   end
   # Return the array of students
   students
@@ -29,8 +33,8 @@ end
 
 # Create method to iterate over array of students. Student info now in a hash, so use key symbols within array [].
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]}) cohort".center(50, " ")
+  students.each do |student, cohort|
+    puts "#{student[:name]} (#{student[:cohort]}) cohort"
   end
 end
 
